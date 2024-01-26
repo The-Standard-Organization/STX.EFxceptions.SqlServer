51acbe43656609d7d1d115c1226f60229cd0c87e
Christo du Toit <christo@dutoit.co.uk>
2024-01-26 18:31:52 +0000

    INFRA: Initial check-in

---
+++ STX.EFxceptions.SqlServer.Infrastructure.Build/Program.cs
@@ -0,0 +1,100 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace STX.EFxceptions.Cosmos.Infrastructure.Build
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using ADotNet.Clients;
+    using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+    using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+    using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV3s;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string branchName = "main";
+            var aDotNetClient = new ADotNetClient();
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = ".Net",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { branchName }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Types = new string[] { "opened", "synchronize", "reopened", "closed" },
+                        Branches = new string[] { branchName }
+                    }
+                },
+
+                EnvironmentVariables = new Dictionary<string, string>
+                {
+                    { "IS_RELEASE_CANDIDATE", EnvironmentVariables.IsGitHubReleaseCandidate() }
+                },
+
+                Jobs = new Dictionary<string, Job>
+                {
+                    {
+                        "build",
+                        new Job
+                        {
+                            RunsOn = BuildMachines.UbuntuLatest,
+
+                            Steps = new List<GithubTask>
+                            {
+                                new CheckoutTaskV3
+                                {
+                                    Name = "Check out"
+                                },
+
+                                new SetupDotNetTaskV3
+                                {
+                                    Name = "Setup .Net",
+
+                                    With = new TargetDotNetVersionV3
+                                    {
+                                        DotNetVersion = "7.0.201"
+                                    }
+                                },
+
+                                new RestoreTask
+                                {
+                                    Name = "Restore"
+                                },
+
+                                new DotNetBuildTask
+                                {
+                                    Name = "Build"
+                                },
+
+                                new TestTask
+                                {
+                                    Name = "Test"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            string buildScriptPath = "../../../../.github/workflows/dotnet.yml";
+            string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            aDotNetClient.SerializeAndWriteToFile(githubPipeline, path: buildScriptPath);
+        }
+    }
+}

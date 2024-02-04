914a2b7bda57e4c0c5046d6e0546325f9d821d02
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-02-04 09:41:50 +0900

    BROKERS: Get Status Code

---
+++ STX.EFxceptions.SqlServer.Base/Brokers/DbErrorBroker/ISqlServerErrorBroker.cs
@@ -0,0 +1,12 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Data.SqlClient;
+using STX.EFxceptions.Interfaces.Brokers.DbErrorBroker;
+
+namespace STX.EFxceptions.SqlServer.Base.Brokers.DbErrorBroker
+{
+    public interface ISqlServerErrorBroker : IDbErrorBroker<SqlException>
+    { }
+}

0475111877b987597c7b6098a62b528c9f59da4c
ZafarUrakov <goliburakov535@gmail.com>
2024-04-29 17:09:23 +0500

    ShouldSaveChangesSuccessfully -> PASS

---
+++ STX.EFxceptions.SqlServer.Tests.Acceptance/EFxceptionsContextTests.Logic.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// --------------------------------------------------------------------------------
+
+using System;
+using STX.EFxceptions.SqlServer.Tests.Acceptance.Models.Clients;
+
+namespace STX.EFxceptions.SqlServer.Tests.Acceptance
+{
+    public partial class EFxceptionsContextTests
+    {
+        [Fact]
+        public void ShouldSaveChangesSuccessfully()
+        {
+            // given
+            var client = new Client
+            {
+                Id = Guid.NewGuid()
+            };
+
+            // when
+            context.Clients.Add(client);
+            context.SaveChanges();
+
+            // then
+            context.Clients.Remove(client);
+            context.SaveChanges();
+        }
+    }
+}

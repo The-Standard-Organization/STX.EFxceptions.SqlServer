14913f3352868d9bef7a205f6e4dc6a1e8d662d9
ZafarUrakov <goliburakov535@gmail.com>
2024-04-29 17:36:30 +0500

    ShouldSaveChangesSuccessfully -> PASS

---
+++ STX.EFxceptions.Identity.SqlServer.Tests.Acceptance/EFxceptionsIdentityContextTests.Logic.cs
@@ -0,0 +1,31 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using STX.EFxceptions.Abstractions.Models.Exceptions;
+using STX.EFxceptions.Identity.SqlServer.Tests.Acceptance.Models.Clients;
+
+namespace STX.EFxceptions.Identity.SqlServer.Tests.Acceptance
+{
+    public partial class EFxceptionsIdentityContextTests
+    {
+        [Fact]
+        public void ShouldSaveChangesSuccessfully()
+        {
+            // given
+            var client = new Client
+            {
+                Id = Guid.NewGuid()
+            };
+
+            // when
+            context.Clients.Add(client);
+            context.SaveChanges();
+
+            // then
+            context.Clients.Remove(client);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ STX.EFxceptions.Identity.SqlServer.Tests.Acceptance/MyEFxceptionsIdentityContext.cs
@@ -0,0 +1,27 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using STX.EFxceptions.Identity.SqlServer.Tests.Acceptance.Models.Clients;
+
+namespace STX.EFxceptions.Identity.SqlServer.Tests.Acceptance
+{
+    public class MyEFxceptionsIdentityContext<TUser, TRole, TKey>
+        : EFxceptionsIdentityContext<TUser, TRole, TKey>
+        where TUser : IdentityUser<TKey>
+        where TRole : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public MyEFxceptionsIdentityContext(DbContextOptions options) : base(options)
+        { }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
